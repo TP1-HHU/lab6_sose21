3faaa3a86fa8a1d29bf5de529346b069edc616ae
Goetz <goetz@deepthought4>
2021-05-26 15:10:13 +0200

    v1.1

---
+++ lorenz_start.cxx
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void RKstep(double* const y1, const double* const y0, const double dt)
+{
+  // Die Werte von u_n stehen im 3er Vektor auf den y0 zeigt.
+  // y1 zeigt auf einen zweiten 3er Vektor, dessen Werte von dieser
+  // Funktion mit den Werten von u_{n+1} überschrieben werden.
+}
+
+void f(double* const y)
+{
+   // y zeigt auf den 3er Vektor der an die Funktion f übergeben wird.
+   // Diese Werte werden mit den Werten f(y) durch diese Funktion überschrieben.
+   // Es wird also kein neuer Vektor in dieser Funktion angelegt und dann
+   // zurückgegeben. 
+}
+
+
+int main(){
+
+
+
+    
+}
